2087fb2ddb686a3ba8cb48248bc3c53196cea225
summerglock <itsautumnsai@gmail.com>
2025-11-22 23:29:24 +0700

    add bottom navigation bar in dashboard page

--- lib/dashboard_page.dart
+++ lib/dashboard_page.dart
@@ -2,16 +2,56 @@ import 'package:flutter/material.dart';
 import 'package:utspam_c3_5b_0703/front_page.dart';
 import 'model/Users.dart';
 
-class DashboardPage extends StatelessWidget {
+class DashboardPage extends StatefulWidget {
   final Users user;
 
   const DashboardPage({super.key, required this.user});
 
   @override
+  State<DashboardPage> createState() => _DashboardPageState();
+}
+
+class _DashboardPageState extends State<DashboardPage> {
+  int _selectedIndex = 0;
+
+  late List<Widget> _widgetOptions;
+
+  @override
+  void initState() {
+    super.initState();
+    _widgetOptions = <Widget>[
+      // Profile Page (Index 0)
+      Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text("Email: ${widget.user.email}"),
+            Text("Phone: ${widget.user.phone}"),
+            Text("Address: ${widget.user.address}"),
+          ],
+        ),
+      ),
+      // Explore Page (Index 1)
+      Center(),
+      // History Page (Index 2)
+      Center(),
+    ];
+  }
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Welcome, ${user.name}!", style: TextStyle(fontSize: 24)),
+        title: Text(
+          "Welcome, ${widget.user.name}!",
+          style: TextStyle(fontSize: 24),
+        ),
         actions: [
           IconButton(
             icon: Icon(Icons.logout),
@@ -25,11 +65,19 @@ class DashboardPage extends StatelessWidget {
         ],
         automaticallyImplyLeading: false,
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [],
-        ),
+      body: _widgetOptions.elementAt(_selectedIndex),
+      bottomNavigationBar: BottomNavigationBar(
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(icon: Icon(Icons.person), label: 'Profile'),
+          BottomNavigationBarItem(icon: Icon(Icons.explore), label: 'Explore'),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.access_time),
+            label: 'Rent History',
+          ),
+        ],
+        currentIndex: _selectedIndex,
+        selectedItemColor: Colors.blueGrey.shade700,
+        onTap: _onItemTapped,
       ),
     );
   }
